8ba7c75c99aac6360710a456255b2193c3ca97eb
EvanMateo7 <gevanmateo@gmail.com>
2021-07-16 21:56:28 -0400

    Complete LikeDislike component and add it to ItemDetails

--- src/app/components/like-dislike/like-dislike.component.ts
+++ src/app/components/like-dislike/like-dislike.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-like-dislike',
@@ -6,12 +7,38 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./like-dislike.component.css']
 })
 export class LikeDislikeComponent implements OnInit {
+  @Input() firestoreDoc: AngularFirestoreDocument;
+  @Input() firestoreDir: string;
+
   @Input() likes: number;
   @Input() dislikes: number;
 
-  constructor() { }
+  doc: any;
+
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    if (this.firestoreDoc) {
+      this.doc = this.firestoreDoc.get().toPromise().then(doc => {
+        this.doc = doc.data();
+      })
+    }
+  }
+
+  like(event: Event) {
+    if (this.doc) {
+      console.log("liking...");
+      this.firestoreDoc.update({likes: (this.doc?.likes || 0) + 1});
+      event.stopPropagation();
+    }
+  }
+
+  dislike(event: Event) {
+    if (this.doc) {
+      console.log("disliking...");
+      this.firestoreDoc.update({dislikes: (this.doc?.dislikes || 0) + 1});
+      event.stopPropagation();
+    }
   }
 
 }

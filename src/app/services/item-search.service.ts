19e17ea73006b93c07c6efdd8d5698511112193f
EvanMateo7 <gevanmateo@gmail.com>
2021-10-10 19:55:33 -0400

    Add item search fallback to firestore + Extract Algolia configs to env files

--- src/app/services/item-search.service.ts
+++ src/app/services/item-search.service.ts
@@ -2,21 +2,30 @@ import { Injectable } from '@angular/core';
 import { Item } from '../models';
 import algoliasearch from 'algoliasearch/lite';
 import { BehaviorSubject } from 'rxjs';
-
-const algoliaClient = algoliasearch('WN5Z9RJLKU', 'fb68ffed3f270354488a2b97d830234a');
-const algoliaIndex = algoliaClient.initIndex('angularapp_dev');
+import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
+import { environment } from "../../environments/environment";
+const AlgoliaConfig = environment.algolia;
 
 @Injectable()
 export class ItemSearchService {
 
   searchQuery: string = '';
   readonly results = new BehaviorSubject<Partial<Item>[]>([]);
+  private algoliaIndex;
   private page: number = 0;
   pages: number[];
   numPages: number = 0;
   totalResults: number = 0;
+  maxPerPage = 20;
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+    this.initAlgolia();
+  }
+
+  private initAlgolia() {
+    const algoliaClient = algoliasearch(AlgoliaConfig.appId, AlgoliaConfig.searchAPIKey);
+    this.algoliaIndex = algoliaClient.initIndex('angularapp_dev');
+  }
 
   get currentPage() {
     return this.page;
@@ -56,18 +65,40 @@ export class ItemSearchService {
     this.pages = Array.from(Array(this.numPages).keys());
   }
 
-  async search(query: string, page: number = 0, numPerPage: number = 20): Promise<void> {
+  async search(query: string, page: number = 0, numPerPage: number = this.maxPerPage): Promise<void> {
     this.searchQuery = query;
-    const result = await algoliaIndex.search(query, {
-      page: page,
-      hitsPerPage: numPerPage
-    });
+    let result;
 
-    this.results.next(result.hits as Partial<Item>[]);
-    this.currentPage = page;
-    this.numPages = result.nbPages;
-    this.totalResults = result.nbHits;
-    this.setPages();
+    try {
+      result = await this.algoliaIndex.search(query, {
+        page: page,
+        hitsPerPage: numPerPage
+      });
+      this.results.next(result.hits as Partial<Item>[]);
+      this.currentPage = page;
+      this.numPages = result.nbPages;
+      this.totalResults = result.nbHits;
+      this.setPages();
+    } catch (error) {
+      console.error("Failed to search Algolia", error)
+      this.getItemsFromFirestore();
+    }
   }
 
+  getItemsFromFirestore() {
+    const itemsCollection: AngularFirestoreCollection<Item> = 
+      this.afs.collection("items", ref => ref.limit(this.maxPerPage));
+
+    itemsCollection.get().toPromise()
+      .then(querySnapshot => {
+        let items: Item[] = querySnapshot.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+        });
+        this.results.next(items);
+      })
+      .catch(err => console.error(err));
+  }
 }
